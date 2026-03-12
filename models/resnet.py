7b8386dfff50b7ca0c9c49a80211a4ff587eb311
Sagar D <sagard.cmagalur@gmail.com>
2026-03-12 18:57:29 +0530

    Add regularization and data augmentation to avoid overfitting

--- models/resnet.py
+++ models/resnet.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 class ResNetBuilder:
 
     def __init__(self):
@@ -15,6 +16,7 @@ class ResNetBuilder:
             strides=1,
             padding="same",
             kernel_initializer="he_normal",
+            kernel_regularizer=tf.keras.regularizers.l2(1e-4),
         )(X)
         X = tf.keras.layers.BatchNormalization(axis=-1)(X)
         X = tf.keras.layers.ReLU()(X)
@@ -25,13 +27,13 @@ class ResNetBuilder:
             strides=1,
             padding="same",
             kernel_initializer="he_normal",
+            kernel_regularizer=tf.keras.regularizers.l2(1e-4),
         )(X)
         X = tf.keras.layers.BatchNormalization(axis=-1)(X)
         X = tf.keras.layers.Add()([X, X_shortcut])
         X = tf.keras.layers.ReLU()(X)
 
         return X
-
 
     def projection_block(self, X, filters, strides=2):
 
@@ -43,6 +45,7 @@ class ResNetBuilder:
             strides=strides,
             padding="same",
             kernel_initializer="he_normal",
+            kernel_regularizer=tf.keras.regularizers.l2(1e-4),
         )(X)
         X = tf.keras.layers.BatchNormalization(axis=-1)(X)
         X = tf.keras.layers.ReLU()(X)
@@ -53,6 +56,7 @@ class ResNetBuilder:
             strides=1,
             padding="same",
             kernel_initializer="he_normal",
+            kernel_regularizer=tf.keras.regularizers.l2(1e-4),
         )(X)
         X = tf.keras.layers.BatchNormalization(axis=-1)(X)
 
@@ -66,15 +70,34 @@ class ResNetBuilder:
 
         return X
 
+    def data_augmentation_block():
 
-    def build_resnet18(self) :
+        data_augmentaion = tf.keras.Sequential(
+            [
+                tf.keras.layers.RandomFlip("horizontal"),
+                tf.keras.layers.RandomRotation(0.1),
+                tf.keras.layers.RandomZoom(0.1),
+            ]
+        )
+        return data_augmentaion
 
-        inputs = tf.keras.layers.Input(shape=(64,64,3))
+    def build_resnet18(self):
 
-        X = tf.keras.layers.Conv2D(filters=64, kernel_size=(7,7), strides=2, padding="same", kernel_initializer="he_normal")(inputs)
+        inputs = tf.keras.layers.Input(shape=(64, 64, 3))
+
+        X = self.data_augmentation_block()(inputs)
+
+        X = tf.keras.layers.Conv2D(
+            filters=64,
+            kernel_size=(7, 7),
+            strides=2,
+            padding="same",
+            kernel_initializer="he_normal",
+            kernel_regularizer=tf.keras.regularizers.l2(1e-4)
+        )(X)
         X = tf.keras.layers.BatchNormalization(axis=-1)(X)
         X = tf.keras.layers.ReLU()(X)
-        X = tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=2, padding="same")(X)
+        X = tf.keras.layers.MaxPool2D(pool_size=(3, 3), strides=2, padding="same")(X)
 
         X = self.identity_block(X, filters=64)
         X = self.identity_block(X, filters=64)
@@ -93,4 +116,3 @@ class ResNetBuilder:
 
         model = tf.keras.Model(inputs, outputs)
         return model
-
